23398fa43f0812933a55909032fc5b34a018a9e2
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2024-04-14 23:46:45 +0700

    chore: [chapter-checker] move cron time to env + Feat: [chapter-checker] add multiple manga support (#15)

--- tasks/NewChapterChecker.js
+++ tasks/NewChapterChecker.js
@@ -2,6 +2,13 @@ const { readFile, writeFile } = require("node:fs/promises");
 const axios = require("axios");
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
+const crontabScheduleChapterChecker = process.env.CHAPTER_CHECKER_CRONTAB_SCHEDULE;
+var crontabSchedule = crontabScheduleChapterChecker
+
+if (typeof crontabSchedule === 'undefined' && crontabSchedule !== null) {
+    crontabSchedule = "* * * * *"; // Set schedule to default
+}
+
 async function getMangaByID(mangaID) {
 	try {
 		const res = await axios({
@@ -15,57 +22,58 @@ async function getMangaByID(mangaID) {
 }
 
 async function checkNewChapter(channel) {
-	const mangaID = "acdbf57f-bf54-41b4-8d92-b3f3d14c852e";
-	let latestChapter = 0;
-	try {
-		latestChapter = await readFile("./latest.txt", { encoding: "utf-8" });
-		console.log(latestChapter);
-	} catch (error) {
-		console.warn("Never checked chapter before");
-	}
+	const mangaIDList = ['e0ee7a1c-db6c-4cc8-927b-ae3a6609ee37'];
 
-	try {
-		const mangaName = await getMangaByID(mangaID);
-		console.log(mangaName);
-		const res = await axios({
-			method: "GET",
-			url: `https://api.mangadex.org/manga/${mangaID}/feed`,
-			params: {
-				translatedLanguage: ["en", "id"],
-				order: {
-					chapter: "desc"
-				},
-				limit: 1
-			}
-		});
-
-		if (res.data.data[0].attributes.chapter > latestChapter) {
-			latestChapter = res.data.data[0].attributes.chapter;
-			writeFile("./latest.txt", latestChapter);
-		} else {
-			return;
+	mangaIDList.forEach(async function(mangaID) {
+		let latestChapter = 0;
+		try {
+			latestChapter = await readFile("./latest.txt", { encoding: "utf-8" });
+			console.log(latestChapter);
+		} catch (error) {
+			console.warn("Never checked chapter before");
 		}
-
-		const embed = new EmbedBuilder()
-			.setTitle(`Update Manga ${mangaName}`)
-			.setDescription(`Chapter ${latestChapter} is now available!`)
-			.setTimestamp();
-
-		const btnRead = new ButtonBuilder()
-			.setLabel("Read")
-			.setURL(`https://mangadex.org/title/${mangaID}`)
-			.setStyle(ButtonStyle.Link);
-
-		const row = new ActionRowBuilder()
-			.addComponents(btnRead);
-		
-		channel.send({embeds: [embed], components: [row]});
-	} catch (error) {
-		console.error(error);
-	}
+		try {
+			const mangaName = await getMangaByID(mangaID);
+			console.log(mangaName);
+			const res = await axios({
+				method: "GET",
+				url: `https://api.mangadex.org/manga/${mangaID}/feed`,
+				params: {
+					translatedLanguage: ["en", "id"],
+					order: {
+						chapter: "desc"
+					},
+					limit: 1
+				}
+			});
+			if (parseFloat(res.data.data[0].attributes.chapter) > latestChapter) {
+				latestChapter = res.data.data[0].attributes.chapter;
+				writeFile("./latest.txt", latestChapter);
+			} else {
+				return;
+			}
+	
+			const embed = new EmbedBuilder()
+				.setTitle(`Update Manga ${mangaName}`)
+				.setDescription(`Chapter ${latestChapter} is now available!`)
+				.setTimestamp();
+	
+			const btnRead = new ButtonBuilder()
+				.setLabel("Read")
+				.setURL(`https://mangadex.org/title/${mangaID}`)
+				.setStyle(ButtonStyle.Link);
+	
+			const row = new ActionRowBuilder()
+				.addComponents(btnRead);
+			
+			channel.send({embeds: [embed], components: [row]});
+		} catch (error) {
+			console.error(error);
+		}
+	})
 }
 
 module.exports = {
-	crontab: "* * * * *",
+	crontab: crontabSchedule,
 	execute: checkNewChapter
 };
